5e0a812dd8d16cb51fd7fab23a1f2d0c676e147f
Chris Panican <chrispanican@gmail.com>
2018-12-13 23:07:36 -0500

    Remove all files

--- lecture1/1.4.scm
+++
@@ -1,9 +0,0 @@
-; Exercise 1.4. Observe that our model of evaluation allows for combinations
-; whose operators are compound expressions. Use this observation to describe
-; the behavior of the following procedure:
-
-(define (a-plus-abs-b a b)
-  ((if (> b 0) + -) a b))
-
-; This function takes values a and b and returns the sum of
-; a and absolute value of b.
--- lecture2/1.6.scm
+++
@@ -1,3 +0,0 @@
-; If Alyssa used new-if on square root program, then
-; the program will run in an infinite loop because
-; new-if will keep calling sqrt-iter from else-clause.
