a51313652a1c213b37fee925e0896eb7344049bb
Gerry Xu <gerryxu.zqx@gmail.com>
2018-12-16 20:16:48 -0500

    initalized a pairing heap and started implementation of isHeapEmpty

--- pairing-heap.scm
+++ pairing-heap.scm
@@ -1,1 +1,15 @@
 ;; base file
+
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (delay b)))))
+
+(define heap (cons-stream 12 (cons-stream 7 (cons-stream 4 '()))))
+
+
+;; check if the heap is empty
+(define (isHeapEmpty? pheap)
+  (null? pheap))
+
+(isHeapEmpty? heap)
