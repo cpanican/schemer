f19d97c99b5a69724805435f0a3840c3569cfa0d
Gerry Xu <gerryxu.zqx@gmail.com>
2018-12-17 22:03:20 -0500

    added stream car and stream cdr and display the cons stream of the heap

--- pairing-heap.scm
+++ pairing-heap.scm
@@ -8,11 +8,18 @@
 (define heap (cons-stream 12 (cons-stream 7 (cons-stream 4 '()))))
 
 
+; creates an empty heap
+(define (empty-heap)
+  '())
+
 ;; check if the heap is empty
 (define (isHeapEmpty? pheap)
-  (null? pheap))
+  (if (equal? (empty-heap) pheap)
+      #t
+      #f
+      ))
 
-(isHeapEmpty? heap)
+(isHeapEmpty? (cdr heap))
 
 
 ;; insert
@@ -24,7 +31,7 @@
 
 
 ;; find-min
-;; (define (find-min ))
+;;(define (find-min pheap)
 
 
 ;; delete-min
@@ -33,3 +40,27 @@
 
 ;; merge-pairs
 ;; (define (merge-pairs ))
+
+
+; we can view streams using display-stream
+(define stream-null? null?)
+
+(define (stream-car stream) (car stream))
+
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      '()
+      (begin
+	(proc (stream-car s))
+	(stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(display-stream heap)
