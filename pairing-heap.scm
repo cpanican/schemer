6dd2e61b9ef2abf8e303e9aadd9df7c19207a7a0
Chris Panican <chrispanican@gmail.com>
2018-12-17 20:40:49 -0500

    Add some pseudocode

--- pairing-heap.scm
+++ pairing-heap.scm
@@ -1,10 +1,10 @@
-;; base file
-
-
+;; streams
 (define-syntax cons-stream
   (syntax-rules ()
     ((_ a b) (cons a (delay b)))))
 
+
+;; make heap
 (define heap (cons-stream 12 (cons-stream 7 (cons-stream 4 '()))))
 
 
@@ -13,3 +13,23 @@
   (null? pheap))
 
 (isHeapEmpty? heap)
+
+
+;; insert
+;; (define (insert ))
+
+
+;; merge
+;; (define (merge ))
+
+
+;; find-min
+;; (define (find-min ))
+
+
+;; delete-min
+;; (define (delete-min ))
+
+
+;; merge-pairs
+;; (define (merge-pairs ))
